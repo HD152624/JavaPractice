cb62513a61fe2d1b6c01b0add0045a5b07a29aec
Administrator <Administrator@WIN-S7K071M0MBB>
2017-05-16 10:50:24 +0900

    실습과제 11

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *     | _ SmartPhoneTest
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 16.
+ * </pre>
+ *
+ * @author 		: Administrator
+ * @version		: 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SmartPhone[] s = {
+				new IPhone("iPhone 7", "애플", 900000),
+				new Galaxy("갤럭시 S8", "삼성", 800000)
+		};
+		
+		
+		for(SmartPhone s1 : s) {
+				System.out.println(s1.toString());
+				s1.turnOn();
+				s1.pay();
+				s1.useSpecialFunction(s1);
+				s1.turnOff();
+				System.out.println();
+
+		}
+
+	}
+
+}
